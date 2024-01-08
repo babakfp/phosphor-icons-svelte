364c5e0fcb4bf713c47f1e51765985e30395bbeb
babakfp <babak.bxf@gmail.com>
2024-01-08 06:25:49 -0800

    Updated test page content

--- kit/src/routes/+page.svelte
+++ kit/src/routes/+page.svelte
@@ -1,1 +1,13 @@
-<h1>Hello, World!</h1>
+<script>
+    // heart (Regular)
+    import IconHeartRegular from "$lib/IconHeartRegular.svelte"
+
+    // heart-break (Bold)
+    import IconHeartBreakBold from "$lib/IconHeartBreakBold.svelte"
+</script>
+
+<!-- heart (Regular) -->
+<IconHeartRegular />
+
+<!-- heart-break (Bold) -->
+<IconHeartBreakBold />
