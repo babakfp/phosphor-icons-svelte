98b6af7651e9d1a33a9b2c14688a35b9e5c9f879
babakfp <babak.bxf@gmail.com>
2024-12-06 07:30:28 +0330

    Removed duplicate

--- src/utilities/convertToComponent.ts
+++ src/utilities/convertToComponent.ts
@@ -5,9 +5,8 @@ const script = `<script lang="ts">
 </script>`
 
 export const convertToComponent = async (svg: string, iconName: string) => {
-    svg = svg.replace('fill="currentColor" ', "") // Removing this because it's redundant.
     const attrs =
-        `class={class_} data-phosphor-icon="${iconName}" aria-hidden="true" width="1em" height="1em" fill="currentColor" pointer-events="none" display="inline-block"`
+        `class={class_} data-phosphor-icon="${iconName}" aria-hidden="true" width="1em" height="1em" pointer-events="none" display="inline-block"`
     svg = svg.replace("<svg", `<svg ${attrs}`)
 
     const minScript = await minify(script, { minifyJS: true })
