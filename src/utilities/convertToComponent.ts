9ce4fadd1ef04bb1054bc73056de4c246dc907bf
babakfp <babak.bxf@gmail.com>
2024-12-06 06:43:03 +0330

    fix: move styles to attributes
    By removing the `style` tag and moving the style properties to attributes, we fix the CSS specificity issue in Svelte. This also prevents fetching a CSS file for these styles.

--- src/utilities/convertToComponent.ts
+++ src/utilities/convertToComponent.ts
@@ -4,23 +4,12 @@ const script = `<script lang="ts">
     let { class: class_ }: { class?: string } = $props()
 </script>`
 
-const style = `<style>
-    svg {
-        width: 1em;
-        height: 1em;
-        fill: currentColor;
-        pointer-events: none;
-        display: inline-block;
-    }
-</style>`
-
 export const convertToComponent = async (svg: string, iconName: string) => {
     svg = svg.replace('fill="currentColor" ', "") // Removing this because it's redundant.
-    const attrs = `class={class_} data-phosphor-icon="${iconName}" aria-hidden="true"`
+    const attrs = `class={class_} data-phosphor-icon="${iconName}" aria-hidden="true" width="1em" height="1em" fill="currentColor" pointer-events="none" display="inline-block"`
     svg = svg.replace("<svg", `<svg ${attrs}`)
 
-    const minStyle = await minify(style, { minifyCSS: true })
     const minScript = await minify(script, { minifyJS: true })
 
-    return minScript + svg + minStyle
+    return minScript + svg
 }
