0dbaafc747e159eb82dde78efe41e277e1e4330d
babakfp <babak.bxf@gmail.com>
2024-12-06 07:49:15 +0330

    Revert "Simpler component output"
    This reverts commit 2461f71f1a2882881567240de585a835fdfd0331.

--- src/utilities/convertToComponent.ts
+++ src/utilities/convertToComponent.ts
@@ -1,12 +1,12 @@
 import { minify } from "html-minifier-terser"
 
 const script = `<script lang="ts">
-    const props: { class?: string } = $props()
+    let { class: class_ }: { class?: string } = $props()
 </script>`
 
 export const convertToComponent = async (svg: string, iconName: string) => {
     const attrs =
-        `class={props.class} data-phosphor-icon="${iconName}" aria-hidden="true" width="1em" height="1em" pointer-events="none" display="inline-block"`
+        `class={class_} data-phosphor-icon="${iconName}" aria-hidden="true" width="1em" height="1em" pointer-events="none" display="inline-block"`
     svg = svg.replace("<svg", `<svg ${attrs}`)
 
     const minScript = await minify(script, { minifyJS: true })
